ac77a4729ade98b26e28aeb5dc87b5950537fc95
Israel Araujo <isra.rar@icloud.com>
2019-05-03 20:17:58 -0300

    Endpoint pedidos

---
+++ src/main/java/com/isra/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.isra.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.isra.cursomc.domain.Pedido;
+import com.isra.cursomc.repository.PedidoRepository;
+import com.isra.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo; 
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
